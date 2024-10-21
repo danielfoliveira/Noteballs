86dbe3db4b8859735ddbdea4d96c2210ff7bacf9
Daniel Oliveira <danielfoliveira13@gmail.com>
2024-10-21 19:09:37 +0100

    Delete note alert modal

--- src/components/Notes/ModalDeleteNote/ModalDeleteNote.vue
+++ src/components/Notes/ModalDeleteNote/ModalDeleteNote.vue
@@ -1,32 +1,17 @@
 <template>
-  <div class="modal is-active">
-    <div class="modal-background"></div>
-    <div
-      class="modal-card"
-      ref="modalCard"
-    >
-      <header class="modal-card-head">
+  <div class="modal">
+    <div class="modal-background" />
+    <div class="modal-card" ref="modalCard">
+      <header class="modal-card-header">
         <p class="modal-card-title">Delete Note?</p>
-        <button
-          class="delete"
-          aria-label="close"
-          @click="closeModal"
-        />
+        <span class="close" aria-label="close" @click="closeModal">&times;</span>
       </header>
       <section class="modal-card-body">
         Are you sure you want to delete this note?
       </section>
-      <footer class="modal-card-foot is-justify-content-flex-end">
-        <button
-          class="button"
-          @click="closeModal"
-        >
-          Cancel
-        </button>
-        <button 
-          class="button is-danger"
-          @click="storeNotes.deleteNote(id)"
-        >
+      <footer class="modal-card-footer">
+        <button class="button-secondary mr-1" @click="closeModal">Cancel</button>
+        <button class="button is-danger" @click="storeNotes.deleteNote(id)">
           Delete
         </button>
       </footer>
@@ -35,49 +20,52 @@
 </template>
 
 <script setup>
-import { onClickOutside } from '@vueuse/core'
-import { onMounted, onUnmounted, ref } from 'vue'
-import { useStoreNotes } from '@/stores/storeNotes'
+import { onClickOutside } from "@vueuse/core";
+import { onMounted, onUnmounted, ref } from "vue";
+import { useStoreNotes } from "@/stores/storeNotes";
 
 // store
-const storeNotes = useStoreNotes()
-const { deleteNote } = storeNotes 
+const storeNotes = useStoreNotes();
+const { deleteNote } = storeNotes;
 
 // props
 const props = defineProps({
   modelValue: {
     type: Boolean,
-    default: false
+    default: false,
   },
   id: {
     type: String,
-    required: true
-  }
-})
+    required: true,
+  },
+});
 
 // Emits
-const emit = defineEmits(['update:modelValue'])
+const emit = defineEmits(["update:modelValue"]);
 
 // close modal
 const closeModal = () => {
-  emit('update:modelValue', false)
-}
+  emit("update:modelValue", false);
+};
 
 // Clickoutside
-const modalCard = ref(null)
-onClickOutside(modalCard, closeModal)
+const modalCard = ref(null);
+onClickOutside(modalCard, closeModal);
 
 // keyboard controll
-const handleKeyboard = e => {
-  if (e.key === 'Escape') closeModal()
-}
+const handleKeyboard = (e) => {
+  if (e.key === "Escape") closeModal();
+};
 
 onMounted(() => {
-  document.addEventListener('keyup', handleKeyboard)
-})
+  document.addEventListener("keyup", handleKeyboard);
+});
 
 onUnmounted(() => {
-  document.removeEventListener('keyup', handleKeyboard)
-})
-
+  document.removeEventListener("keyup", handleKeyboard);
+});
 </script>
+
+<style scoped>
+@import "./ModalDeleteNote.css";
+</style>
